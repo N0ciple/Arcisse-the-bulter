9032af111f0e752ebc41087c4b95af90ea3379e7
Robin Dupont <robin.dpnt@gmail.com>
2017-08-27 14:34:51 +0200

    modify command detection
    Code refactor as well

--- arcisse.js
+++ arcisse.js
@@ -2,6 +2,8 @@ const Discord = require("discord.js");
 var mysql = require('mysql');
 var config = require('./config.json')
 const client = new Discord.Client();
+var prefix = '!';
+
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -20,7 +22,13 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+
 client.on('message', msg => {
+
+  // Ignore message from bots
+  if(message.author.bot) return;
+
+  // Save messages in the database
   var sql = "INSERT INTO messages SET ?";
   var post = {content: msg.content,
               creation: msg.createdAt,
@@ -33,10 +41,16 @@ client.on('message', msg => {
     console.log("Message from "+ post.author +" created at " + post.creation + " was successfully saved");
     });
 
-  if (msg.content === 'ping') {
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+
+  if (command === 'ping') {
       msg.channel.send('Je suis Arcisse et je fais miskine!');
   }
- if(msg.content === "qsdfqsdfqez") {
+
+ /*if(msg.content === "qsdfqsdfqez") {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     const sayMessage = msg.content;
@@ -45,8 +59,9 @@ client.on('message', msg => {
     // And we get the bot to say the thing: 
     msg.channel.send("author "+ msg.author + " createdat " + msg.createdAt + " timestamp" + msg.createdTimestamp +" content "+ msg.content + " id "+msg.id+ " member "+msg.member);
     console.log(msg.member + " "+msg.author);
-  }
- if(msg.content === 'stat arcissesdfsdfsdfds'){
+  }*/
+
+ if(command === 'stats'){
   var sql ="SELECT COUNT(*) FROM messages WHERE author=?";
   var total = 0;
 
